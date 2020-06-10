4550aeb8a43de6c1aefd4afa9d0c3185e789bb76
Anthony Johnson <58479386+anthonyj713@users.noreply.github.com>
2020-06-10 13:35:55 -0600

    add head and tail on doubly linked list works and passes tests

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -48,7 +48,16 @@ class DoublyLinkedList:
     as the new head of the list. Don't forget to handle 
     the old head node's previous pointer accordingly."""
     def add_to_head(self, value):
-        pass
+        new_node = ListNode(value, None, self.head)
+
+        if self.head is not None:
+            self.head = new_node
+            self.length += 1
+        else:
+            self.head.insert_before(new_node)
+            self.head = self.head.prev
+            self.length += 1
+            
 
     """Removes the List's current head node, making the
     current head's next node the new head of the List.
@@ -60,7 +69,15 @@ class DoublyLinkedList:
     as the new tail of the list. Don't forget to handle 
     the old tail node's next pointer accordingly."""
     def add_to_tail(self, value):
-        pass
+        new_node = ListNode(value, self.tail, None)
+
+        if self.tail is not None:
+            self.tail = new_node
+            self.length += 1
+        else:
+            self.tail.insert_after(new_node)
+            self.tail = self.tail.prev
+            self.length += 1
 
     """Removes the List's current tail node, making the 
     current tail's previous node the new tail of the List.
